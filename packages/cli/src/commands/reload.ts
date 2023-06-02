5b59eca706fc743f8b76bcdcd412452e2bfd2053
Joram van den Boezem <joram@vandenboezem.nl>
2023-06-02 12:43:00 +0200

    feat: recursive reload

--- packages/cli/src/commands/reload.ts
+++ packages/cli/src/commands/reload.ts
@@ -1,6 +1,10 @@
 import {
   formatTask,
+  getWorkspaces,
+  isMonorepo,
+  loadAllPlugins,
   loadPluginsTask,
+  task,
   updateDependenciesTask,
 } from "@mokr/core";
 import { command } from "bandersnatch";
@@ -8,14 +12,35 @@ import { resolve } from "node:path";
 
 export const reload = command("reload")
   .description("Reload plugins in repo or workspace")
+  .option("recursive", {
+    type: "boolean",
+    description: "Also reinstalls plugins of all workspaces in a monorepo",
+  })
   .option("cwd", {
     description: "Directory to use as the current working directory",
     default: process.cwd(),
   })
-  .action(async ({ cwd }) => {
+  .action(async ({ recursive, cwd }) => {
     const directory = resolve(cwd);
 
     await loadPluginsTask({ directory });
+
+    if (recursive && (await isMonorepo({ directory }))) {
+      const workspaces = await getWorkspaces({ directory });
+
+      for (const workspace of workspaces) {
+        const workspaceDirectory = resolve(directory, workspace.location);
+
+        // Skip root
+        if (workspaceDirectory === directory) {
+          continue;
+        }
+
+        await task(`Load plugins of ${workspace.name}`, () =>
+          loadAllPlugins({ directory: workspaceDirectory })
+        );
+      }
+    }
 
     await updateDependenciesTask({ directory });
 
