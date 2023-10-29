46b85e14c4eb2f4fc9fe8bb6f5652335fec50102
Joram van den Boezem <joram@vandenboezem.nl>
2023-10-29 13:32:03 +0000

    fix: add --worktree

--- packages/plugins/src/jest/jest.ts
+++ packages/plugins/src/jest/jest.ts
@@ -59,9 +59,9 @@ async function install({ directory }: PluginArgs) {
       directory: monorepoDirectory,
       data: {
         scripts: {
-          test: "yarn workspaces foreach --topological --verbose run test",
+          test: "yarn workspaces foreach --worktree --topological --verbose run test",
           "test:watch":
-            "yarn workspaces foreach --parallel --interlaced run test:watch",
+            "yarn workspaces foreach --worktree --parallel --interlaced run test:watch",
         },
       },
     });
